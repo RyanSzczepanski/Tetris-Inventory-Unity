75407d04569716b31deba3e0141c418d80d990cf
RyanSzczepanski <Slyry11@gmail.com>
2024-02-27 21:18:44 -0500

    Set Up Sub Inventory Iterm Event Args

---
+++ Assets/RyanSzczepanski/Inventory/Systems/Scripts/SubInventoryItemEventArgs.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubInventoryItemEventArgs : EventArgs
+{
+    public Item Item { get; set; }
+    public Vector2Int OriginGridCoordinate { get; set; }
+}
