b40336decb9ff3fd3e70868102d1d8cad7bccb30
RyanSzczepanski <Slyry11@gmail.com>
2024-02-27 21:19:04 -0500

    Set Up Inventory

---
+++ Assets/RyanSzczepanski/Inventory/Systems/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    //Inventory Refrences
+    public readonly SubInventory[] subInventories;
+
+    //InventoryData should come from Inventory Interface
+    public Inventory(/*InventoryData*/)
+    {
+        //Foreach (InventoryData.SubInventories SubInvData){
+        //  new SubInventory(SubInvData, this)
+        //}
+    }
+    //AddItem(Item)
+    //RemoveItem(Item)
+}
