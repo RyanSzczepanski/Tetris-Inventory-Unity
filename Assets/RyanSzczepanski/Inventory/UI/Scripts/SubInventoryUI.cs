7f67aaffcb3ba2e2e09e01c8b5722a112935565e
RyanSzczepanski <Slyry11@gmail.com>
2024-04-27 20:06:17 -0400

    added sub inventory events

--- Assets/RyanSzczepanski/Inventory/UI/Scripts/SubInventoryUI.cs
+++ Assets/RyanSzczepanski/Inventory/UI/Scripts/SubInventoryUI.cs
@@ -8,12 +8,53 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class SubInventoryUI : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    public SubInventory SubInventory { get; private set; }
 
-    public void Init(SubInventory subInventory)
+    public SubInventory SubInventory { get; private set; }
+    public InventoryCellDrawSettingsSO drawSettingsSO;
+
+    public void Init(SubInventory subInventory, InventoryCellDrawSettingsSO drawSettingsSO)
     {
         SubInventory = subInventory;
+        this.drawSettingsSO = drawSettingsSO;
+        Subscribe();
+        foreach (Slot slot in  SubInventory.Slots)
+        {
+            if (slot.IsOccupied)
+            {
+                //ItemUI.InitUI(slot.ItemInSlot, this);
+            }
+        }
     }
+
+    private void Subscribe()
+    {
+        SubInventory.ItemAdded += OnItemAdded;
+        SubInventory.ItemMoved += OnItemMoved;
+        SubInventory.ItemRemoved += OnItemRemoved;
+    }
+
+    private void Unsubscribe()
+    {
+        SubInventory.ItemAdded -= OnItemAdded;
+        SubInventory.ItemMoved -= OnItemMoved;
+        SubInventory.ItemRemoved -= OnItemRemoved;
+    }
+
+    public void OnItemAdded(object source, SubInventoryItemAddedEventArgs args)
+    {
+        ItemUI.Init(args.Item, this, args.TargetCellCoordinate);
+
+    }
+    public void OnItemMoved(object source, SubInventoryItemMovedEventArgs args)
+    {
+        
+        }
+    public void OnItemRemoved(object source, SubInventoryItemRemovedEventArgs args)
+    {
+
+    }
+
+
 
     public void OnPointerClick(PointerEventData eventData)
     {
