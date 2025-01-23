00212eed20e167fd284e369af0acff5d709983e5
RyanSzczepanski <Slyry11@gmail.com>
2025-01-22 21:05:40 -0500

    removed non complete editor files

--- Assets/RyanSzczepanski/Inventory/UI/UIImageGenerator/Scripts/InventoryCellDrawSettingsSO.cs
+++ Assets/RyanSzczepanski/Inventory/UI/UIImageGenerator/Scripts/InventoryCellDrawSettingsSO.cs
@@ -3,34 +3,34 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "InventoryUI/DrawSettings", fileName = "New SubInventory UI Draw Settings")]
 public class InventoryCellDrawSettingsSO : ScriptableObject
 {
-    public int _cellSize = 50;
-    public int _paddingSize = 1;
-    public int _outlineSize = 1;
+    public int      _cellSize       = 50;
+    public int      _paddingSize    = 1;
+    public int      _outlineSize    = 1;
 
-    public Color _cellColor = new(0.1f, 0.1f, 0.1f, 0.85f);
-    public Color _cellAccentColor = new(0.15f, 0.15f, 0.15f, 0.85f);
+    public Color    _cellColor              = new(0.10f, 0.10f, 0.10f, 0.85f);
+    public Color    _cellAccentColor        = new(0.15f, 0.15f, 0.15f, 0.85f);
 
-    public Color _cellOutlineColor = new(0.3f, 0.3f, 0.3f, 0.75f);
-    public Color _cellOutlineAccentColor = new(0.4f, 0.4f, 0.4f, 0.85f);
+    public Color    _cellOutlineColor       = new(0.30f, 0.30f, 0.30f, 0.75f);
+    public Color    _cellOutlineAccentColor = new(0.40f, 0.40f, 0.40f, 0.85f);
 
-    public Color _outlineColor = new(0.9f, 0.9f, 0.9f, 0.5f);
-    public Color _outlineAccentColor = new(0.85f, 0.85f, 0.85f, 0.6f);
+    public Color    _outlineColor           = new(0.90f, 0.90f, 0.90f, 0.50f);
+    public Color    _outlineAccentColor     = new(0.85f, 0.85f, 0.85f, 0.60f);
 }
 
 public struct InventoryCellDrawSettings
 {
-    public int   _cellSize;
-    public int   _paddingSize;
-    public int   _outlineSize;
+    public int      _cellSize;
+    public int      _paddingSize;
+    public int      _outlineSize;
 
-    public Color _cellColor;
-    public Color _cellAccentColor;
+    public Color    _cellColor;
+    public Color    _cellAccentColor;
 
-    public Color _cellOutlineColor;
-    public Color _cellOutlineAccentColor;
+    public Color    _cellOutlineColor;
+    public Color    _cellOutlineAccentColor;
 
-    public Color _outlineColor;
-    public Color _outlineAccentColor;
+    public Color    _outlineColor;
+    public Color    _outlineAccentColor;
 
     public InventoryCellDrawSettings(InventoryCellDrawSettingsSO drawSettings)
     {
